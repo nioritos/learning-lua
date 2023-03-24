8ccf26cf60148700176b93bbf1e8801dd0863440
nioritos <junioradr2008@gmail.com>
2023-03-24 12:34:26 -0300

    for

---
+++ module-01/for.lua
@@ -0,0 +1,13 @@
+
+local lista = {
+    [1] = "junin",
+    [2] = "stonks",
+    [3] = "lele",
+    [4] = "rocha",
+    [5] = "deni",
+};
+
+for k,v in pairs(lista) do
+    print(v, "valor")
+    print(k, "indice")
+end
